a704573fe03b8a21d7c23ad33b411a1f283eaab0
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2026-02-26 23:55:30 -0300

    Adiciona arquivos da primeira aula sobre TAD

---
+++ BAG-TAD/bag.c
@@ -0,0 +1,67 @@
+#include"bag.h"
+
+// Inicializa a Bag vazia
+void init(Bag *b){
+	int i;
+	for (i = 0; i < BAGSIZE; i++)
+		b->item[i] = -1;
+}
+
+// Retorna o numero de itens na Bag
+int size(Bag *b){
+	int i, count = 0;
+	
+	for (i = 0; i < BAGSIZE; i++){
+		if (b->item != -1)
+			count++;
+	}
+	return count;
+}
+
+// Insere um elemento na Bag. Retorna 1 para insercao com sucesso ou 0 para Bag cheia.
+int insert(Bag *b, int elem){
+	int i;
+	
+	for (i = 0; i < BAGSIZE; i++) {
+		if (b->item == -1) {
+			b->item[i] = elem;
+			return 1;
+		}	
+	}
+	return 0;	
+}
+
+// Verifica quantas vezes o elemento elem esta na Bag
+int check(Bag *b, int elem){
+	int i, count = 0;
+	
+	for (i = 0; i < BAGSIZE && b->item[i] !=-1; i++) {
+		if (b->item == elem)
+			count++;
+	}
+	return count;
+}
+
+// Remove o elemento elem da Bag. Retorna 1 se a remocao ocorreu com sucesso ou 0 se o elemento nao foi encontrado na Bag.
+int delete(Bag *b, int elem){
+	int i, count = 0;
+	
+	for (i = 0; i < BAGSIZE && b->item[i] !=-1; i++) {
+		if (b->item == elem) {
+			b->item[i] = -1;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// Imprime todos os elementos da Bag
+void printall(Bag b){
+	int i;
+	
+	for (i = 0; i < BAGSIZE && b.item != -1; i++) {
+		printf("%i\n", b.item[i]);
+	}
+	
+}
+
